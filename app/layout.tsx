3d0d284c634e89b761de3ce653042802b6bb0d76
mdjameel04 <jameeltony67@gmail.com>
2025-12-03 00:46:29 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import {ClerkProvider} from '@clerk/nextjs'
+import Provider from "./provider";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,7 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Provider>
         {children}
+        </Provider>
+
+        
       </body>
     </html>
     </ClerkProvider>
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 
+import { Button } from "@/components/ui/button";
 import { UserButton, useUser } from "@clerk/nextjs";
 import { motion } from "motion/react";
+import { div } from "motion/react-client";
+import Link from "next/link";
 
 export default function HeroSectionOne() {
   return (
@@ -19,7 +22,7 @@ export default function HeroSectionOne() {
       </div>
       <div className="px-4 py-10 md:py-20">
         <h1 className="relative z-10 mx-auto max-w-4xl text-center text-2xl font-bold text-slate-700 md:text-4xl lg:text-7xl dark:text-slate-300">
-          {"🩺Revolutionize patient Care with AI Voice Agents"
+          {"🧠 Transform Healthcare With AI Medical Voice Agents"
             .split(" ")
             .map((word, index) => (
               <motion.span
@@ -50,8 +53,9 @@ export default function HeroSectionOne() {
           }}
           className="relative z-10 mx-auto max-w-xl py-4 text-center text-lg font-normal text-neutral-600 dark:text-neutral-400"
         >
-        Deliver instant, accurate medical assistance through natural voice concversations. Automate appointment scheduling, sympton triage, and follow-up care-24/7
+        Deliver instant, accurate medical assistance through natural voice concversations. Automate appointment scheduling, sympton triage, and follow-up care-24/l
         </motion.p>
+        <Link href={"/sign-in"}>
         <motion.div
           initial={{
             opacity: 0,
@@ -66,12 +70,11 @@ export default function HeroSectionOne() {
           className="relative z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
         >
           <button className="w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
-            Explore Now
+            Get Started
           </button>
-          <button className="w-60 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-black transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-900">
-            Contact Support
-          </button>
+        
         </motion.div>
+        </Link>
         <motion.div
           initial={{
             opacity: 0,
@@ -110,10 +113,15 @@ const Navbar = () => {
         <div className="size-7 rounded-full bg-gradient-to-br from-violet-500 to-pink-500" />
         <h1 className="text-base font-bold md:text-2xl">Aceternity UI</h1>
       </div>
-      {!user?  <button className="w-24 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 md:w-32 dark:bg-white dark:text-black dark:hover:bg-gray-200">
+      {!user? 
+      
+      <button className="w-24 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 md:w-32 dark:bg-white dark:text-black dark:hover:bg-gray-200">
         Login
-      </button>:
-      <UserButton/>
+      </button>  :
+      <div className="flex gap-5 items-center">
+        <UserButton/>
+ <Button> Dashboard</Button>
+      </div>
       }
     </nav>
   );
