7c0a1176b5059e8a6b8442362574a04f7d98341a
mdjameel04 <jameeltony67@gmail.com>
2025-12-02 23:11:56 +0530

    first commit

--- components/hero-section-demo-1.tsx
+++ components/hero-section-demo-1.tsx
@@ -5,7 +5,7 @@ import { motion } from "motion/react";
 
 export default function HeroSectionOne() {
   return (
-    <div className="relative  my-10 flex  flex-col items-center justify-center">
+    <div className="relative mx-auto my-10 flex max-w-7xl flex-col items-center justify-center">
       <Navbar />
       <div className="absolute inset-y-0 left-0 h-full w-px bg-neutral-200/80 dark:bg-neutral-800/80">
         <div className="absolute top-0 h-40 w-px bg-gradient-to-b from-transparent via-blue-500 to-transparent" />
@@ -18,7 +18,7 @@ export default function HeroSectionOne() {
       </div>
       <div className="px-4 py-10 md:py-20">
         <h1 className="relative z-10 mx-auto max-w-4xl text-center text-2xl font-bold text-slate-700 md:text-4xl lg:text-7xl dark:text-slate-300">
-          {"🩺Revolutionize patient Care with AI Voice Agents"
+          {"Launch your website in hours, not days"
             .split(" ")
             .map((word, index) => (
               <motion.span
@@ -49,7 +49,9 @@ export default function HeroSectionOne() {
           }}
           className="relative z-10 mx-auto max-w-xl py-4 text-center text-lg font-normal text-neutral-600 dark:text-neutral-400"
         >
-        Deliver instant, accurate medical assistance through natural voice concversations. Automate appointment scheduling, sympton triage, and follow-up care-24/7
+          With AI, you can launch your website in hours, not days. Try our best
+          in class, state of the art, cutting edge AI tools to get your website
+          up.
         </motion.p>
         <motion.div
           initial={{
