22f22d890321403a056180f642fb438fc779ab38
mdjameel04 <jameeltony67@gmail.com>
2025-12-03 19:35:43 +0530

    inital commit

---
+++ app/(routes)/dashboard/_components/DoctorsAgentList.tsx
@@ -0,0 +1,23 @@
+import { AIDoctorAgents } from '@/shared/list'
+import { div } from 'motion/react-client'
+import React from 'react'
+import DoctorAgentCard from './DoctorAgentCard'
+
+function DoctorsAgentList() {
+  return (
+    <div className='mt-10'> 
+      <h2 className='font-bold text-xl'> AI Specialist Doctor </h2>
+     
+     <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mt-5' >
+        {AIDoctorAgents.map((doctor,index)=>(
+            <div key={index}>
+  <DoctorAgentCard doctorAgent={doctor} />
+            </div>
+        ))}
+     </div>
+      
+    </div>
+  )
+}
+
+export default DoctorsAgentList
